fb8bc49c4e12ebaf5170ced08934f82e55f440b7
anusha12anu <anushak0012@gmail.com>
2026-08-20 12:16:02 +0530

    Update Q2.cpp

--- pr2/Q2.cpp
+++ pr2/Q2.cpp
@@ -1,1 +1,26 @@
-
+#include<iostream>
+using namespace std;
+class demo
+{
+    private:
+    int a;
+    public:
+    void accept();
+    void display();
+};
+void demo::accept()
+{
+    cout<<"Enter number: "<<endl;
+    cin>>a;
+}
+void demo::display()
+{
+    cout<<"Number is "<<a;
+}
+int main()
+{
+    demo d;
+    d.accept();
+    d.display();
+    return 0;
+}
