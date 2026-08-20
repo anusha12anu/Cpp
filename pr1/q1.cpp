0ab7efe69ff079755e25e14d4bd9fc611debf690
anusha12anu <anushak0012@gmail.com>
2026-08-20 10:00:17 +0530

    Create q1.cpp

---
+++ pr1/q1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Includes the input-output library
+
+int main() {
+    // Prints text to the screen followed by a new line
+    std::cout << "Hello, World!" << std::endl; 
+    
+    return 0; // Signals that the program ran successfully
+}
+
