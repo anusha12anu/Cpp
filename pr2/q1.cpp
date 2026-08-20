c4c8b675132ff9d4c1ee099af2db5306b29c4d0a
anusha12anu <anushak0012@gmail.com>
2026-08-20 12:07:53 +0530

    Update q1.cpp

--- pr2/q1.cpp
+++ pr2/q1.cpp
@@ -1,1 +1,10 @@
-iwaebsaiudybned
+#include<iostream>
+using namespace std;
+int x=10;
+int main()
+{
+    int x=20;
+    cout<<"Local variable= "<<x<<endl;
+    cout<<"Local variable= "<<::x;
+    return 0;
+}
